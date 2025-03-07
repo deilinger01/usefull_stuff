3a599ffc2648180550d15d549e97c153acdb88d5
Dimitri Eilinger <115653356+deilinger01@users.noreply.github.com>
2025-03-07 18:11:34 +0100

    Update merge_pdf.py
    other variant to merge

--- pdf_merger/merge_pdf.py
+++ pdf_merger/merge_pdf.py
@@ -1,5 +1,6 @@
 import PyPDF2
 import os
+import fitz
 
 def merge_pdfs(input_folder, output_file):
     if os.path.exists(input_folder) == True:
@@ -20,6 +21,16 @@ def merge_pdfs(input_folder, output_file):
     else:
         print('Input path doesnt exist.')
 
+def merge_pdfs_fitz(input_folder, output_file):
+    doc = fitz.open()
+    pdf_files = sorted([file for file in os.listdir(input_folder) if file.endswith(".pdf")])
+
+    for pdf_file in pdf_files:
+        file_path = os.path.join(input_folder, pdf_file)
+        doc.insert_pdf(fitz.open(file_path))
+
+    doc.save(output_file)
+    print("Merged the PDFs using pymupdf.")
 
 input_folder = r""
 output_file = r""
